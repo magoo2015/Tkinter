fdeb95813b70e35222b62c031b851f471eb27354
Sydney McGee <mcgee.sydney@gmail.com>
2023-08-05 23:08:04 -0500

    Initial project setup.  Created main.py file and imported tkinter

---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First Gui Program")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+window.mainloop()
