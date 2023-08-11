4394fbaa1bad474ec4ac946806a2eeb8229c31fb
Sydney McGee <mcgee.sydney@gmail.com>
2023-08-11 14:33:42 -0500

    Created main.py file.  Added imports and created title.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My First Gui Program")
-window.minsize(width=500, height=300)
-
-#Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack()
-
-
-
-
+window = Tk()
+window.title("Miles to Kilometer Converter")
 
 
 window.mainloop()
