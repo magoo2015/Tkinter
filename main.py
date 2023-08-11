f861c5a16bc8cd0b00c7e6c468b29cde3ec76591
Sydney McGee <mcgee.sydney@gmail.com>
2023-08-11 14:38:38 -0500

    Created widgets for app.

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ window.title("Miles to Kilometer Converter")
 
 
 window.mainloop()
+
+miles_input = Entry()
+miles_label = Label(text="Miles")
+
+is_equal_label = Label(text="is equal to")
+
+kilometer_results_label = Label(text="0")
+
+kilometer_label = Label(text="Km")
+
+calculate_button = Button(text="Calculate")
